9450f4d77f2bcecee8b0a737892c04e28142b5b8
Mabdallah11 <abdallahmac9@gmail.com>
2024-01-31 20:51:49 +0300

    Added client.

--- client/urls.py
+++ client/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns =[
     path('', views.client_list, name='clients_list'),
+    path('<int:pk>/', views.clients_detail, name='clients_detail'),
+    path('add/', views.clients_add, name='clients_add'),
 ]
--- client/views.py
+++ client/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages 
 from django.contrib.auth.decorators import login_required 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect 
 
+from .forms import AddCLientForm
 from.models import Client 
 
 @login_required
@@ -10,4 +12,32 @@ def client_list(request):
 
     return render(request, 'client/clients_list.html',{
         'clients': clients
+    })
+
+@login_required
+def clients_detail(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    return render(request, 'client/clients_detail.html',{
+        'client': client
+    })
+
+@login_required
+def clients_add(request):
+    if request.method =='POST':
+        form = AddCLientForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            messages.success(request, 'The client was created.')
+
+            return redirect('client_list')
+    else:
+        form = AddCLientForm()
+
+    return render(request, 'client/clients_add.html', {
+        'form': form 
     })
