bc4849db20b9650e0c524f9a8962735fb20f8b6b
Mabdallah11 <abdallahmac9@gmail.com>
2024-02-04 20:10:17 +0300

    created my account page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,10 +3,11 @@ from django.contrib.auth import views
 from django.urls import path
 
 
-from userprofile.views import signup
+from userprofile.views import signup, myaccount 
 
 urlpatterns = [
     path('sign-up/', signup, name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
-    path('log-out/', views.LogoutView.as_view(), name='logout')
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
+    path('dashboard/myaccount/', myaccount,name='myaccount'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 from .models import Userprofile
+
+from team.models import Team 
 
 def signup(request):
     if request.method == 'POST':
@@ -11,6 +14,9 @@ def signup(request):
             user = form.save()
 
             userprofile = Userprofile.objects.create(user=user)
+            team = Team.objects.create(name='The team name', created_by=request.user)
+            team.members.add(request.user)
+            team.save()
             #userprofile.save
             return redirect('/log-in/')
     else:
@@ -18,5 +24,12 @@ def signup(request):
 
     return render(request, 'userprofile/signup.html',{
         'form':form
+    })
+
+@login_required
+def myaccount(request):
+    team = Team.objects.filter(created_by=request.user)[0]
+    return render(request, 'userprofile/myaccount.html',{
+        'team':team 
     })
 
